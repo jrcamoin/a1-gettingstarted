5b598e366bb4648290ee87207c76b57def25b7b8
Jacob <jrcamoin@wpi.edu>
2026-08-23 15:30:30 -0400

    a1-gettingstarted assignment complete before deployment

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const server = http.createServer( function( request,response ) {
     case '/index.html':
       sendFile( response, 'index.html' )
       break
+    case '/styling.css':
+      sendFile( response, 'styling.css')
+      break
     default:
       response.end( '404 Error: File Not Found' )
   }
